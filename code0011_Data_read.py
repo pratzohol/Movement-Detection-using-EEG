576c7d948512d305bd48a30f47890e29f3209748
Jay Moghariya <moghariyajay@gmail.com>
2022-03-02 00:23:44 +0530

    csv file convertion and 61 col selection

--- code0011_Data_read.py
+++ code0011_Data_read.py
@@ -4,19 +4,44 @@ Created on Fri Mar 26 22:31:29 2021
 
 @author: INFINITE-WORKSTATION
 """
+from opcode import hasjabs
 import mne
 
 #Just display errors and warnings
 mne.set_log_level("WARNING")
 
 #Loading EEG data
-data = mne.io.read_raw_gdf("motorimagination_subject1_run1.gdf", preload=True) 
+data = mne.io.read_raw_gdf("S01_MI/motorimagination_subject1_run1.gdf", preload=True) 
+# mne.viz.plot_raw(data, start=0, duration=10)
+# df=data.to_data_frame()
+# df.to_excel("S01_MI/motorimagination_subject1_run1.xlsx")
+# print(df.head())
+# print(df.info())
+# print(df.describe())
+# print(df.columns)
+all_channels=data.ch_names
+print(all_channels)
+#select channels
+# eeg_data = data.pick_channels(all_channels[:62])
+# print(eeg_data.info())
+print("-"*80)
+# print(eeg_data.ch_names)
+# print("-"*80)
 
 eeg = data.get_data()
 info = data.info
 event_info = data._annotations.description
 event_onset = data._annotations.onset
 time_values = data.times
+print(eeg.shape)
+print("-"*80)
+print(info)
+print("-"*80)
+print(event_info)
+print("-"*80)
+print(event_onset)
+print("-"*80)
+print(time_values)
 
 """
 Event description
